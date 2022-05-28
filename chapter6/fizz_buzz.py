03a2e106b905daae0eb126d64f884a1366be13d9
QuangAnh321 <littledragon1998@gmail.com>
2022-05-28 14:56:56 +0700

    updated to section 6 chapter 173

--- chapter6/fizz_buzz.py
+++ chapter6/fizz_buzz.py
@@ -18,5 +18,29 @@ def fizz_buzz(number: int) -> str:
         return str(number)
 
 
+# for i in range(1, 101):
+#     print(fizz_buzz(i))
+
+
+# Pseudocode
+# Player and computer take turns, game end when user guess wrong or the number to guess reached 100
+
+# Computer start first, and from 1
+# => Computer print result for odd number
+# => Player print result for even number
+# if player is wrong / reached 100 => break
+
 for i in range(1, 101):
-    print(fizz_buzz(i))
+    if i % 2 == 0:
+        player_answer = input(f"The number is {i}, please enter your answer: ")
+        correct_answer = fizz_buzz(i)
+
+        if player_answer == correct_answer:
+            print("Correct!")
+            print("Computer turn next!")
+        else:
+            print("Ops! Wrong answer. Game Over")
+            break
+    else:
+        print(f"The number is {i}, the computer answer is: {fizz_buzz(i)}")
+        print("Your turn next!")
